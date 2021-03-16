c597795789198bb6e034f7e801e6b069fb7b451c
Dominik Lewczyński <dominiklew200060@gmail.com>
2021-03-16 19:51:17 +0100

    Zrobienie zadanie 11 i 12 w folderze lab2

--- lab_2/Zadanie-11.py
+++ lab_2/Zadanie-11.py
@@ -12,4 +12,50 @@
 #   o
 # itd.
 
-#TODO
+wysokosc = 0
+while(wysokosc < 3 or wysokosc > 9):
+    print('!!! wysokość musi być nie mniejsza niż 3 i nie większa od 9 !!!')
+    wysokosc = int(input('podaj wysokość: '))
+wysokosc_parzysta = False
+if(wysokosc % 2 == 0):
+    wysokosc_parzysta = True
+    wysokosc = wysokosc - 1
+###################################
+# gora diamentu
+#   |
+#  \/
+spacje = wysokosc // 2
+kule = 1
+for i in range (0, wysokosc // 2, 1):
+    for j in range (0, spacje, 1):
+        print(' ',end='')
+    spacje = spacje - 1
+    for j in range (0, kule, 1):
+        print('o',end='')
+    kule = kule + 2
+    print()
+###################################
+# srodek diamentu
+#   |
+#  \/
+for i in range (0, wysokosc, 1):
+    print('o', end='')
+print()
+if(wysokosc_parzysta == True):
+    for i in range (0, wysokosc, 1):
+        print('o', end='')
+    print()
+###################################
+# dol diamentu
+#   |
+#  \/
+spacje = 1
+kule = wysokosc - 2
+for i in range (0, wysokosc // 2, 1):
+    for j in range (0, spacje, 1):
+        print(' ',end='')
+    spacje = spacje + 1
+    for j in range (kule, 0, -1):
+        print('o',end='')
+    kule = kule - 2
+    print()
--- lab_2/Zadanie-12.py
+++ lab_2/Zadanie-12.py
@@ -1,3 +1,23 @@
 # Zadanie 12
 # Napisz skrypt, który wyświetla i oblicza tabliczkę mnożenia od 1 do 100 w formie znanej z lekcji matematyki w szkole podstawowej.
-# TODO
+
+print("|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|")
+print('|  *  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  10 |')
+print("|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|")
+for i in range (1, 11, 1):
+    if(len(str(i)) == 1):
+        print('|  '+str(i)+'  |',end='')
+    else:
+        print('|  '+str(i)+' |',end='')
+    for j in range (1, 11, 1):
+        liczba = int(i)*int(j)
+        #######
+        if len(str(liczba)) == 1:
+            print('  '+str(liczba)+'  |', end='')
+        elif len(str(liczba)) == 2:
+            print('  '+str(liczba)+' |', end='')
+        else:
+            print(' '+str(liczba)+' |', end='')
+        #######
+    print()
+    print('|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|')
